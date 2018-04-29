48ddf277dfb4c4a4f0e8a61b5383a3274fb90357
mridulkumar <mk1514@ic.ac.uk>
2018-04-29 15:45:06 +0100

    creates a uvspace but isn't correct.

--- createUV.py
+++ createUV.py
@@ -4,6 +4,8 @@ import math
 import numpy as np
 from numpy import array
 import time
+import xml.etree.ElementTree as ET
+from toolz.dicttoolz import valmap
 
 start_time = time.time()
 
@@ -61,7 +63,8 @@ def createUVSpace():
     im.save("uv.png")
 
 def createUVSpace2(pathToPLY, pathToTexture):
-    uvImage = np.zeros([4096, 4096, 3],dtype=np.uint8)
+
+    uvImage = np.zeros([1024, 1024, 3],dtype=np.uint8)
     uvImage.fill(0)
 
     with open(pathToPLY) as f:
@@ -82,45 +85,78 @@ def createUVSpace2(pathToPLY, pathToTexture):
         for i in range(numberOfVerticies):
             coords = f.readline().replace(" \n", "").split(" ")
             coords = map(float, coords)
+            # coords = np.dot(rotationMatrix, coords).tolist()[0]
             verticies.append(coords)
 
         indexToUV = {}
 
-        for i in range(numberOfFaces):
+        # for i in range(numberOfFaces):
+        while(True):
             line = f.readline()
+            if line == '':
+                break
             line = line.split(" ")
             indexes = line[1:4]
             indexes = map(int, indexes)
             UVCoords = line[5:-1]
             UVCoords = map(float, UVCoords)
-
             for j in range(0, 3):
-                indexToUV[indexes[j]] = [UVCoords[j*2], UVCoords[j*2 + 1]]
+                index = str(indexes[j])
+                indexToUV[index] = [UVCoords[j*2], UVCoords[j*2 + 1]]
 
         textureImage = array(Image.open(pathToTexture))
+        us = []
+        vs = []
 
-        for vertex in range(0, numberOfVerticies):
+        keys = indexToUV.keys()
+        coords = []
+
+        for key in keys:
+            vertex = int(key)
             x = verticies[vertex][0]
             y = verticies[vertex][1]
             z = verticies[vertex][2]
+            coords.append(indexToUV[key])
 
-            u = math.atan2(x,z)
-            u = u/math.pi
-            u = u + 1
-            u = u / 2
-            u = u * 4095
-            u = int(u)
+            u = np.arctan(x/z)
+            us.append(u)
 
-            v = y + 1
-            v = v / 2
-            v = v * 4095
-            print(v)
-            v = int(v)
+            v = y
+            vs.append(v)
+
+        minU = min(us)*-1
+
+        newUs = map(lambda x: x + minU, us)
+        maxU = max(newUs)
+        newUs = map(lambda x: x / maxU, newUs)
+        newUs = map(lambda x: int(x*1023), newUs)
+
+        minV = min(vs)*-1
+
+        newVs = map(lambda x: x + minV, vs)
+        maxV = max(newVs)
+        newVs = map(lambda x: x / maxV, newVs)
+        newVs = map(lambda x: int(x*1023), newVs)
+
+        # # print(len(us), len(newVs))
+        newCoords = zip(newUs, newVs, coords)
+
+        for coord in newCoords:
+            # print(coord)
+            u = coord[0]
+            v = coord[1]
+            
+            tU = coord[2][0] * 4096
+            tV = coord[2][1] * 4096
+
+            R = textureImage[int(tV)][int(tU)][0]
+            G = textureImage[int(tV)][int(tU)][1]
+            B = textureImage[int(tV)][int(tU)][2]
 
             uvImage[u][v][0] = 255
             uvImage[u][v][1] = 255
             uvImage[u][v][2] = 255
-            
+
         im = Image.fromarray(uvImage.astype('uint8'))
         im.save("uv2.png")
 
