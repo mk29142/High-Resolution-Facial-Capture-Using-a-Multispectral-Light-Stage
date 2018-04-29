98c416148515e71de8a8c51ba26a9c20d75cd4ca
mridulkumar <mk1514@ic.ac.uk>
2018-04-29 11:46:15 +0100

    bug where Vs range is very small.

--- createUV.py
+++ createUV.py
@@ -3,6 +3,9 @@ from PIL import Image
 import math
 import numpy as np
 from numpy import array
+import time
+
+start_time = time.time()
 
 def createUVSpace():
 
@@ -57,5 +60,72 @@ def createUVSpace():
     im = Image.fromarray(uvImage.astype('uint8'))
     im.save("uv.png")
 
+def createUVSpace2(pathToPLY, pathToTexture):
+    uvImage = np.zeros([4096, 4096, 3],dtype=np.uint8)
+    uvImage.fill(0)
+
+    with open(pathToPLY) as f:
+        numberOfVerticies = 0
+        numberOfFaces = 0
+
+        while(True):
+            line = f.readline()
+            if "end_header" in line:
+                break
+            if "element vertex" in line:
+                numberOfVerticies = int(line.split(" ")[-1])
+            if "element face" in line:
+                numberOfFaces = int(line.split(" ")[-1])
+        
+        verticies = []
+
+        for i in range(numberOfVerticies):
+            coords = f.readline().replace(" \n", "").split(" ")
+            coords = map(float, coords)
+            verticies.append(coords)
+
+        indexToUV = {}
+
+        for i in range(numberOfFaces):
+            line = f.readline()
+            line = line.split(" ")
+            indexes = line[1:4]
+            indexes = map(int, indexes)
+            UVCoords = line[5:-1]
+            UVCoords = map(float, UVCoords)
+
+            for j in range(0, 3):
+                indexToUV[indexes[j]] = [UVCoords[j*2], UVCoords[j*2 + 1]]
+
+        textureImage = array(Image.open(pathToTexture))
+
+        for vertex in range(0, numberOfVerticies):
+            x = verticies[vertex][0]
+            y = verticies[vertex][1]
+            z = verticies[vertex][2]
+
+            u = math.atan2(x,z)
+            u = u/math.pi
+            u = u + 1
+            u = u / 2
+            u = u * 4095
+            u = int(u)
+
+            v = y + 1
+            v = v / 2
+            v = v * 4095
+            print(v)
+            v = int(v)
+
+            uvImage[u][v][0] = 255
+            uvImage[u][v][1] = 255
+            uvImage[u][v][2] = 255
+            
+        im = Image.fromarray(uvImage.astype('uint8'))
+        im.save("uv2.png")
+
 if __name__ == "__main__":
-    createUVSpace()
+    # createUVSpace()
+    createUVSpace2("lowresForUV.ply", "textureForUV.png")
+
+    print("--- %s seconds ---" % (time.time() - start_time))
